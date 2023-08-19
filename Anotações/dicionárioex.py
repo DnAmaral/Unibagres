8da568140a1167d7ca327305e9393398e8de23c4
unknown <tad-daniloamaral@ucpparana.edu.br>
2023-08-18 22:01:37 -0300

    Dicionário e Atualizações!

--- Anotações/dicionárioex.py
+++ Anotações/dicionárioex.py
@@ -1,7 +1,10 @@
-dados_cliente = [
+dados_cliente = {
     "Nome": "Danilo",
     "Endereço": "Rua ABC, 123",
     "Telefone": "948696918"
-]
+}
 
 print (dados_cliente['Nome'])
+
+dados_cliente["Cidade"] = "Ivaiporã"
+print (dados_cliente)
