25c4f6e87a34ef5efb848ea8bf14b36301b220c6
unknown <tad-daniloamaral@ucpparana.edu.br>
2023-08-18 21:43:19 -0300

    Listas e Tuplas!

--- Anotações/listaex.py
+++ Anotações/listaex.py
@@ -1,4 +1,15 @@
-lista1 = ('Danilo', 'Silva', 'Amaral')
+lista1 = ("Danilo", "Silva", "Amaral", "É", "Maneiro")
 print(type(lista1))
 print(len(lista1))
 print(lista1[2])
+
+lista2 = lista1 *2
+
+print(lista2)
+
+
+
+#É adicionado no final da lista!
+
+lista1.append("SouEu")
+print(lista1)
