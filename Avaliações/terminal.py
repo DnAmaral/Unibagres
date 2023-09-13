f06ca684eb8fe558aee34ffaeb24870d3f4b4da6
Danilo Amaral <tad-daniloamaral@ucpparana.edu.br>
2023-09-13 12:37:51 -0300

    Avaliação Terminal

--- Avaliações/terminal.py
+++ Avaliações/terminal.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
+# Função para exibir a mensagem em uma nova janela;
 def enviar_mensagem():
     nome = nome_entry.get()
     mensagem = mensagem_entry.get()
@@ -11,19 +12,23 @@ def enviar_mensagem():
     else:
         messagebox.showerror("Erro", "Por favor, preencha ambos os campos.")
 
+# Criar a janela principal;
 root = tk.Tk()
 root.title("Envio de Mensagem")
 
+# Label e campo de entrada para o nome
 nome_label = tk.Label(root, text="Nome:")
 nome_label.pack()
 nome_entry = tk.Entry(root)
 nome_entry.pack()
 
+# Label e campo de entrada para a mensagem
 mensagem_label = tk.Label(root, text="Mensagem:")
 mensagem_label.pack()
 mensagem_entry = tk.Entry(root)
 mensagem_entry.pack()
 
+# Botão "Enviar" para exibir a mensagem
 enviar_button = tk.Button(root, text="Enviar", command=enviar_mensagem)
 enviar_button.pack()
 
